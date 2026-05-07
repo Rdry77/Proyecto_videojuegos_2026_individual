70d96baded974f44517dbe884a3d34e06ed5f290
Rdry77 <rdry210@gmail.com>
2026-05-07 16:02:07 -0500

    Configuracion inicial del enemigo

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,25 +1,36 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED_NORMAL = 250.0
+@export var SPEED_ATTACK = 550.0
+@export var SPEED_DIAGONAL = 900.0
+@export var JUMP_FORCE = -600.0
+@export var ATTACK_COOLDOWN = 3.5     # Tiempo entre decisiones de ataque
+@export var TURN_DELAY = 1.0          # Retraso para girar (1 segundo)
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+# Asegúrate de que tu nodo jugador se llame "Player" en la escena
+@onready var player = get_tree().root.find_child("Player", true, false)
+
+# --- variables de estado ---
+var current_state = "NORMAL"
+var attack_timer = 2.0
+var turn_timer = 0.0
+var last_direction = -1
+var strike_done = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and current_state != "AIR_ATTACK":
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED_NORMAL
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED_NORMAL)
 
 	move_and_slide()
