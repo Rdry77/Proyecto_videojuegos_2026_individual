2a7b99bc03dd098a7629525333179f930e47535b
Rdry77 <rdry210@gmail.com>
2026-05-19 01:36:00 -0500

    Definicion de variables y configuracion del callback physics_process

--- scripts/enemy_main.gd
+++ scripts/enemy_main.gd
@@ -1,25 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED_NORMAL = 350.0 # Más rápido
+@export var SPEED_ATTACK = 700.0 # Embestida letal
+@export var SPEED_DIAGONAL = 1000.0 
+@export var JUMP_FORCE = -700.0
+@export var ATTACK_COOLDOWN = 1.5 # Ataca muy seguido
+@export var TURN_DELAY = 0.2     # Gira casi instantáneamente
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var player = get_tree().root.find_child("Player", true, false)
+
+# --- ESTADOS ---
+var current_state = "NORMAL"
+var esta_herido = false
+var esta_muerto = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and current_state != "AIR_ATTACK":
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if esta_muerto:
+		velocity.x = move_toward(velocity.x, 0, 10.0)
+	elif esta_herido:
+		velocity.x = move_toward(velocity.x, 0, 15.0)
 
 	move_and_slide()
